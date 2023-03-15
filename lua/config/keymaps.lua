b3848fa963392ce5e1db6f20f14bd5a085202241
AkihitoNakano <linkin_087@yahoo.co.jp>
2023-03-16 00:54:34 +0900

    fix: Move to kemaps command to keymaps directory

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+-- general keymaps
+
+-- "j"と"k"でESCの代わりができる
+keymap.set("i", "jk", "<ESC>")
+
+-- normal modeでnhでハイライトをクリアする
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- カットした文字をレジスタに保存しないようにする
+keymap.set("n", "x", '"_x')
